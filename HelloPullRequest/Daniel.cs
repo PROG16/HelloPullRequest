13473c0fb649af3ca82f7a93964a5c270a49e31b
DannyBoy <d_iliovits@windowslive.com>
2017-10-31 15:35:47 +0100

    Added Daniel class with method Speak.

---
+++ HelloPullRequest/Daniel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPullRequest
+{
+    public class Daniel
+    {
+        public string Speak()
+        {
+            return "Here I am!";
+        }
+    }
+}
