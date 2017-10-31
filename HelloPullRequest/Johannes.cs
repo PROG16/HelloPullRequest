3c0cee8babbbc1c9beffb1e63b6b67a44fdc7a3c
JochoVoled <johannes@smidelov.com>
2017-10-31 15:43:34 +0100

    Included Albins file in csproj, and added my own file

---
+++ HelloPullRequest/Johannes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPullRequest
+{
+    class Johannes : Person
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("There is no cow level");
+        }
+    }
+}
