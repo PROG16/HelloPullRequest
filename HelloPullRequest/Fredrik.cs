2205735f4d9a75065b31892eccd8e03b7759f835
Fredrik Haglund <mejl@fredrikhaglund.se>
2017-10-31 15:03:16 +0100

    Add Fredrik Speak

--- HelloPullRequest/Fredrik.cs
+++ HelloPullRequest/Fredrik.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace HelloPullRequest
 {
-    class Program
+    class Fredrik
     {
-        static void Main(string[] args)
+        public static void Speak()
         {
-            Fredrik.Speak();
+            Console.WriteLine("Fredrik waz here!");
         }
     }
 }
