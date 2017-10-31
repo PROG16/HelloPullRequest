63c632d4963a1233f7f0e1639dc05a93d659bc8e
isabellaalstrom <isabella.alstrom@gmail.com>
2017-10-31 15:35:51 +0100

    Added class Isa and made it speak in Program.cs

---
+++ HelloPullRequest/Isa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPullRequest
+{
+    class Isa : Person
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Isabella!");
+        }
+    }
+}
