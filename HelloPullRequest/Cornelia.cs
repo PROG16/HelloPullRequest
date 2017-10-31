e025fa76b4f26a535bf128c21651e2e51cc9d4e3
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-10-31 15:36:39 +0100

    Add Cornelia.cs

---
+++ HelloPullRequest/Cornelia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloPullRequest
+{
+    class Cornelia
+    {
+        public static void Jump()
+        {
+            Console.WriteLine("Jump up and down");
+        }
+    }
+}
